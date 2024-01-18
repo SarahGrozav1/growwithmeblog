3af06b43e490d90dd9a8ed4d35fff03174a480fc
Sarah <grozavsarah@gmail.com>
2024-01-18 13:09:58 +0000

    Set debug to false

--- grow/settings.py
+++ grow/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'django-insecure-thl70$u+)j3iose6)pd08@jcyxx+^taj649y%zg@csc5!kc@@b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['growwithme-33cbf82d7376.herokuapp.com', 'localhost',
                 '8000-sarahgrozav-growwithmeb-4pf7yd6lg7s.ws-eu107.gitpod.io']
