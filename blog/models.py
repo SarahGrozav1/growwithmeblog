41dbfe892790056305a3a203af2559de057ef579
Sarah <grozavsarah@gmail.com>
2024-01-23 19:51:09 +0000

    Delete about model

--- blog/models.py
+++ blog/models.py
@@ -68,10 +68,4 @@ class CollaborateRequest(models.Model):
         return f"Collaboration request from {self.name}"
 
 
-class About(models.Model):
-    title = models.CharField(max_length=200)
-    updated_on = models.DateTimeField(auto_now=True)
-    content = models.TextField()
 
-    def __str__(self):
-        return self.title
